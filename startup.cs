8ac6b9029718bad982cf033dc0f6504506a4dc63
Sushant Poudel <104451877+sushantpoudel58@users.noreply.github.com>
2023-01-07 08:33:22 +0545

    Add files via upload

---
+++ startup.cs
@@ -0,0 +1,56 @@
+﻿namespace TechnoSewa
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddMvc();
+            services.AddProgressiveWebApp();
+
+
+
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+
+            app.MapControllerRoute(
+                name: "my_route",
+                pattern: "{controller=register}/{action=Index}/{id?}");
+           // app.UseSession();
+            app.MapRazorPages();
+
+            app.Run();
+
+        }
+    }
+}
