4fd41c896ec1984b19d1ac37fddcf93ba51c0e93
Sushant Poudel <104451877+sushantpoudel58@users.noreply.github.com>
2023-01-07 10:41:29 +0545

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using TechnoSewa.Models;
+
+namespace TechnoSewa.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TechnoSewa.Models.registerModel> tbl_register { get; set; }
+        public DbSet<TechnoSewa.Models.SubmitProblemModel> tbl_problem { get; set; }
+        public DbSet<TechnoSewa.Models.Book> tbl_book { get; set; }
+
+
+
+    }
+}
